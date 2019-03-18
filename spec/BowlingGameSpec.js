438c6dc581cbaa48a7f4b14e467244c6d88dc1cf
James Ault <james.ault@johnlewis.co.uk>
2019-03-18 10:58:06 +0000

    First test passing for class methods, second test for total score passing

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -2,18 +2,21 @@
 
 describe ("BowlingGame", function (){
   var game;
+  var i;
 
   beforeEach(function () {
     game = new BowlingGame;
   })
 
-  it("should be able to create a new instance of BowlingGame", function(){
+  it('able to call the .score() method and return a score', function(){
     game = new BowlingGame;
-  });
-
-  it('able to call the .roll() method', function() {
     game.roll(0);
+    expect(game.score()).toEqual(0);
   });
 
-
+  it('able to return the total score from a number of rolls', function(){
+    for (i = 0 ; i < 20 ; i++)
+      game.roll(1);
+      expect(game.score()).toEqual(20);
+  })
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,7 +1,11 @@
 function BowlingGame() {
-  
+  this.totalScore = 0
 }
 
 BowlingGame.prototype.roll = function (pins) {
-  
+  this.totalScore += pins
+}
+
+BowlingGame.prototype.score = function () {
+  return this.totalScore
 }
