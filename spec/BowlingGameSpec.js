8adbf395227fa796402d48363a7328fc60bc4f45
James Ault <james.ault@johnlewis.co.uk>
2019-03-18 10:24:54 +0000

    First test for BowlingGameClass

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+describe ("BowlingGame", function (){
+  var game;
+
+  it("should be able to create a new instance of BowlingGame", function(){
+    game = new BowlingGame;
+  });
+
+
+});
